d42f82de4f4d61b00489707b3ad9a562679a24b9
Simon Laing <laingster@gmail.com>
2014-07-09 22:02:54 +0100

    Updated build script

--- build.bat
+++ build.bat
@@ -1,13 +1,30 @@
 @echo off
 
+echo Moving to 'deploy' branch
 git checkout deploy
+
+echo Merging in latest changes
 git merge master
-msbuild FamilyTree.sln
+
+echo Building in Release...
+msbuild FamilyTree.sln /verbosity:minimal /p:Configuration=Release /nologo
 
 if %ERRORLEVEL%==0 (
+	echo Build succeeded, committing binaries
+
 	git add bin/
 	git commit -m "Committed binaries"
 	
+	echo Pushing deployment...
 	git push
+	
+	echo Moving back to 'master' branch
 	git checkout master
+	
+	echo Rebuilding in Debug...
+	msbuild FamilyTree.sln /verbosity:minimal /p:Configuration=Debug /nologo
+	
+	echo Finished.
+) else (
+	echo Build failed.
 )
