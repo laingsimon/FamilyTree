0258202eca8fb6bf7b1e4db08d66c47de0cae358
Simon Laing <Simon.Laing@fundslibrary.co.uk>
2015-02-17 11:00:27 +0000

    Introduced missing ETagHelper file

---
+++ Models/ETagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FamilyTree.Models
+{
+	public class ETagHelper
+	{
+		public static string GetEtagFromFile(FileInfo fileInfo, string customEtagSuffix = null, DateTime? customDateTimePart = null)
+		{
+			var lastWriteTime = fileInfo.LastWriteTimeUtc;
+			var size = fileInfo.Length;
+			var fileName = fileInfo.FullName;
+			var customEtagSuffixHashCode = customEtagSuffix == null ? "" : customEtagSuffix.GetHashCode().ToString();
+			var customDateTimePartHashCode = customDateTimePart == null ? "" : customDateTimePart.GetHashCode().ToString();
+
+			return lastWriteTime.GetHashCode()
+				+ size.GetHashCode()
+				+ fileName.GetHashCode()
+				+ customEtagSuffixHashCode
+				+ customDateTimePartHashCode;
+		}
+	}
+}
