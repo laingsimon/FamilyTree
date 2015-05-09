0e38fed43bbcf2a9fa8809992dd3cfe42d2e4b4f
Simon Laing <laingster@gmail.com>
2015-05-09 09:34:11 +0100

    Introduced details popup

---
+++ Scripts/Family.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(
+	function () {
+		$(".person .details").click(function () {
+			$(this).prev(".details-popup").css("display", "flex");
+		});
+
+		$(".person .details-popup").click(function () {
+			$(this).hide();
+		});
+});
