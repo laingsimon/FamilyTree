bc590ba71ea19215920f0648c83e11e020352773
Simon Laing <laingster@gmail.com>
2015-06-20 20:55:25 +0100

    FileSystem: Introduction of Azure blob-storage FileSystem

---
+++ Models/FileSystem/AzureStorage/AzureStorageFileSystem.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FamilyTree.Models.FileSystem.AzureStorage
+{
+	public class AzureStorageFileSystem : IFileSystem
+	{
+		private const string _containerName = "filesystem";
+		private readonly CloudBlobClient _client;
+		private readonly Uri _baseUri;
+		private readonly CloudBlobContainer _container;
+
+		public AzureStorageFileSystem()
+		{
+			var storageAccount = CloudStorageAccount.Parse(
+				CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+			_client = storageAccount.CreateCloudBlobClient();
+			_container = _client.GetContainerReference(_containerName);
+			_container.CreateIfNotExists();
+
+			_baseUri = new Uri(_container.Uri.ToString() + "/", UriKind.Absolute);
+		}
+
+		private Uri _BuildUri(string path)
+		{
+			if (string.IsNullOrEmpty(path) || path.Length <= 2)
+				throw new ArgumentException("Invalid path: '" + path + "'");
+
+			if (!path.StartsWith("~"))
+				throw new InvalidOperationException("Invalid path, must be relative to app: '" + path + "'");
+
+			var subPath = path.Substring(2);
+
+			return new Uri(_baseUri, subPath);
+		}
+
+		private IEnumerable<string> _GetPathParts(string path)
+		{
+			var parts = path.Split('/').Skip(1).ToArray();
+			var lastPart = parts.Last();
+			var containsFileName = lastPart.Contains(".");
+			var partsToTake = containsFileName
+				? parts.Length - 1
+				: parts.Length;
+
+			foreach (var part in parts.Take(partsToTake))
+				yield return part;
+		}
+
+		private IEnumerable<string> _GetFullPath(IDirectory directory)
+		{
+			if (directory.Parent != null)
+			{
+				foreach (var parent in _GetFullPath(directory.Parent))
+					yield return parent;
+			}
+
+			yield return directory.Name;
+		}
+
+		private IDirectory _GetDirectoryFromPath(IReadOnlyCollection<string> parts)
+		{
+			if (parts.Count() == 0)
+				return null;
+
+			var firstPart = parts.First();
+			var remainingParts = parts.Skip(1);
+
+			return new Directory(
+				firstPart,
+				_GetDirectoryFromPath(remainingParts.ToArray()),
+				this);
+		}
+
+		private IDirectory _GetDirectoryFromPath(string path)
+		{
+			var parts = _GetPathParts(path);
+			return _GetDirectoryFromPath(parts.ToArray());
+		}
+
+		private CloudBlobDirectory _GetAzureDirectory(IFile path)
+		{
+			var relativePath = string.Join("/", _GetFullPath(path.Directory));
+			return _container.GetDirectoryReference(relativePath);
+		}
+
+		private CloudBlobDirectory _GetAzureDirectory(string path)
+		{
+			var relativePath = string.Join("/", _GetPathParts(path));
+			return _container.GetDirectoryReference(relativePath);
+		}
+
+		private ICloudBlob _GetAzureFile(IFile file)
+		{
+			var fullPath = string.Join("/", _GetFullPath(file.Directory)) + "/" + file.Name;
+			return _GetAzureFile(fullPath);
+		}
+
+		private ICloudBlob _GetAzureFile(string path)
+		{
+			var directory = _GetAzureDirectory(path);
+			var fileName = Path.GetFileName(path);
+			var blobItem = (from blob in directory.ListBlobs()
+							let blobFileName = Path.GetFileName(blob.Uri.AbsoluteUri)
+							where blobFileName.Equals(fileName, StringComparison.OrdinalIgnoreCase)
+							select blob).SingleOrDefault();
+
+			return blobItem == null
+				? null
+				: _client.GetBlobReferenceFromServer(blobItem.StorageUri);
+		}
+
+		public IFile GetFile(string path)
+		{
+			var blobRef = _GetAzureFile(path);
+			if (blobRef == null)
+				return null;
+
+			var directory = _GetDirectoryFromPath(path);
+			var lastModified = blobRef.Properties.LastModified ?? DateTimeOffset.MinValue;
+			var length = blobRef.Properties.Length;
+
+			return new File(
+				blobRef.Name,
+				directory,
+				length,
+				lastModified.UtcDateTime,
+				this);
+		}
+
+		public IDirectory GetDirectory(string path)
+		{
+			return _GetDirectoryFromPath(path);
+		}
+
+		public IEnumerable<IFile> GetFiles(IDirectory directory, string searchPattern)
+		{
+			var fullPath = string.Join("/", _GetFullPath(directory));
+			var uri = new Uri(_baseUri, fullPath);
+
+			var azureDirectory = _container.GetDirectoryReference(fullPath);
+			return from item in azureDirectory.ListBlobs()
+				   let file = _client.GetBlobReferenceFromServer(item.StorageUri)
+				   let lastModified = file.Properties.LastModified ?? DateTimeOffset.MinValue
+				   select new File(
+					   file.Name,
+					   directory,
+					   file.Properties.Length,
+					   lastModified.UtcDateTime,
+					   this);
+		}
+
+		public IEnumerable<IDirectory> GetDirectories(IDirectory directory)
+		{
+			return new[] { directory };
+		}
+
+		public Stream OpenRead(IFile file)
+		{
+			var azureFile = _GetAzureFile(file);
+			if (azureFile == null)
+				throw new FileNotFoundException();
+
+			return azureFile.OpenRead();
+		}
+
+		public bool FileExists(string path)
+		{
+			return _GetAzureFile(path) != null;
+		}
+
+		public Stream OpenWrite(IFile file)
+		{
+			var azureFile = _GetAzureFile(file);
+			if (azureFile == null)
+			{
+				var directory = _GetAzureDirectory(file);
+				var reference = directory.GetBlockBlobReference(file.Name);
+
+				//TODO: Create the file - how...
+			}
+
+			return new FileWriteStream(azureFile);
+		}
+
+		public IFile CreateFile(string path)
+		{
+			var uri = _BuildUri(path);
+			return new File(
+				Path.GetFileName(path),
+				_GetDirectoryFromPath(path),
+				0,
+				DateTime.MinValue,
+				this);
+		}
+	}
+}
---
+++ Models/FileSystem/AzureStorage/FileWriteStream.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FamilyTree.Models.FileSystem.AzureStorage
+{
+	public class FileWriteStream : Stream
+	{
+		private readonly ICloudBlob _blobRef;
+		private readonly MemoryStream _memoryStream = new MemoryStream();
+
+		public FileWriteStream(ICloudBlob blobRef)
+		{
+			_blobRef = blobRef;
+		}
+
+		#region read members
+		public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override bool CanRead
+		{
+			get { return false; }
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override System.Threading.Tasks.Task<int> ReadAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override System.Threading.Tasks.Task CopyToAsync(Stream destination, int bufferSize, System.Threading.CancellationToken cancellationToken)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override int EndRead(IAsyncResult asyncResult)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override int ReadByte()
+		{
+			throw new NotSupportedException();
+		}
+
+		public override int ReadTimeout
+		{
+			get { return base.ReadTimeout; }
+			set { base.ReadTimeout = value; }
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override long Position
+		{
+			get
+			{
+				throw new NotSupportedException();
+			}
+			set
+			{
+				throw new NotSupportedException();
+			}
+		}
+
+		public override long Length
+		{
+			get { throw new NotSupportedException(); }
+		}
+		#endregion
+
+		#region write members
+		public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			return _memoryStream.BeginWrite(buffer, offset, count, callback, state);
+		}
+
+		public override bool CanWrite
+		{
+			get { return true; }
+		}
+
+		public override void EndWrite(IAsyncResult asyncResult)
+		{
+			_memoryStream.EndWrite(asyncResult);
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			_memoryStream.Write(buffer, offset, count);
+		}
+
+		public override System.Threading.Tasks.Task WriteAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken)
+		{
+			return _memoryStream.WriteAsync(buffer, offset, count, cancellationToken);
+		}
+
+		public override void WriteByte(byte value)
+		{
+			_memoryStream.WriteByte(value);
+		}
+
+		public override int WriteTimeout
+		{
+			get { return _memoryStream.WriteTimeout; }
+			set { _memoryStream.WriteTimeout = value; }
+		}
+
+		public override void SetLength(long value)
+		{
+			_memoryStream.SetLength(value);
+		}
+		#endregion
+
+		#region other members
+		public override bool CanTimeout
+		{
+			get { return false; }
+		}
+
+		public override bool CanSeek
+		{
+			get { return false; }
+		}
+
+		public override void Flush()
+		{ }
+
+		public override Task FlushAsync(System.Threading.CancellationToken cancellationToken)
+		{
+			return Task.FromResult(0);
+		}
+		#endregion
+
+		protected override void Dispose(bool disposing)
+		{
+			_blobRef.UploadFromStream(_memoryStream);
+		}
+	}
+}
