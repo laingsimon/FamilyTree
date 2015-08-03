945f42824a6f055c28274faa00c87ef91bee505d
Simon Laing <Simon.Laing@fundslibrary.co.uk>
2015-08-03 19:38:37 +0100

    Introduction of a HttpFileSystem

---
+++ Controllers/FileSystemController.cs
@@ -0,0 +1,132 @@
+﻿using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using FamilyTree.Models.Authentication;
+using FamilyTree.Models.FileSystem;
+using FamilyTree.Models.FileSystem.AzureStorage;
+using JsonResult = FamilyTree.Models.JsonResult;
+
+namespace FamilyTree.Controllers
+{
+	[Authorize(Roles = Roles.SuperUser)]
+	public class FileSystemController : Controller
+	{
+		private readonly IFileSystem _fileSystem;
+
+		public FileSystemController()
+			: this(new AzureStorageFileSystem())
+		{ }
+
+		public FileSystemController(IFileSystem fileSystem)
+		{
+			_fileSystem = fileSystem;
+		}
+
+		[HttpGet]
+		public ActionResult File(string path)
+		{
+			if (string.IsNullOrEmpty(path))
+				return HttpNotFound();
+			if (!path.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var file = _fileSystem.GetFile(path);
+			if (file == null)
+				return HttpNotFound();
+
+			return new JsonResult(file);
+		}
+
+		[HttpGet]
+		public ActionResult Directory(string path)
+		{
+			if (string.IsNullOrEmpty(path))
+				return HttpNotFound();
+			if (!path.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var directory = _fileSystem.GetDirectory(path);
+			if (directory == null)
+				return HttpNotFound();
+
+			return new JsonResult(directory);
+		}
+
+		[HttpGet]
+		public ActionResult Files(string directoryPath, string searchPattern)
+		{
+			if (string.IsNullOrEmpty(directoryPath))
+				return HttpNotFound();
+			if (!directoryPath.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var directory = _fileSystem.GetDirectory(directoryPath);
+
+			if (directory == null)
+				return HttpNotFound();
+
+			var files = _fileSystem.GetFiles(directory, searchPattern);
+			if (files == null)
+				return HttpNotFound();
+
+			return new JsonResult(files);
+		}
+
+		[HttpGet]
+		public ActionResult Directories(string directoryPath)
+		{
+			if (string.IsNullOrEmpty(directoryPath))
+				return HttpNotFound();
+			if (!directoryPath.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var directory = _fileSystem.GetDirectory(directoryPath);
+
+			if (directory == null)
+				return HttpNotFound();
+
+			var directories = _fileSystem.GetDirectories(directory);
+			if (directories == null)
+				return HttpNotFound();
+
+			return new JsonResult(directories);
+		}
+
+		[HttpGet]
+		public ActionResult FileContent(string path)
+		{
+			if (string.IsNullOrEmpty(path))
+				return HttpNotFound();
+			if (!path.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var file = _fileSystem.GetFile(path);
+			if (file == null)
+				return HttpNotFound();
+
+			return new FileStreamResult(file.OpenRead(), "application/octet-stream");
+		}
+
+		[HttpPost]
+		public ActionResult FileContent(string path, Stream newContent)
+		{
+			if (string.IsNullOrEmpty(path))
+				return HttpNotFound();
+			if (!path.StartsWith("~"))
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			if (newContent == null)
+				newContent = Request.InputStream;
+			if (newContent == null)
+				return new HttpStatusCodeResult(HttpStatusCode.LengthRequired);
+
+			var file = _fileSystem.GetFile(path);
+			if (file == null)
+				return HttpNotFound();
+
+			using (var writeStream = file.OpenWrite())
+				newContent.CopyTo(writeStream);
+
+			return new HttpStatusCodeResult(HttpStatusCode.OK);
+		}
+	}
+}
---
+++ Models/FileSystem/HttpFileSystem.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Configuration;
+using Newtonsoft.Json;
+
+namespace FamilyTree.Models.FileSystem
+{
+	public class HttpFileSystem : IFileSystem
+	{
+		private static readonly Uri _configuredBaseUri = _GetConfiguredBaseUri();
+
+		private readonly Uri _baseUri;
+		private readonly JsonSerializer _serialiser;
+		private readonly WebClient _webClient;
+
+		public HttpFileSystem(Uri baseUri = null, ICredentials credentials = null, JsonSerializer serialiser = null)
+		{
+			_baseUri = baseUri ?? _configuredBaseUri;
+			_serialiser = serialiser ?? new JsonSerializer();
+
+			_webClient = new WebClient
+			{
+				Credentials = credentials
+			};
+		}
+
+		private static Uri _GetConfiguredBaseUri()
+		{
+			var baseUri = WebConfigurationManager.AppSettings["HttpFileSystem.BaseUri"];
+			if (baseUri == null)
+				throw new InvalidOperationException("HttpFileSystem.BaseUri is not configured");
+			if (!baseUri.EndsWith("/"))
+				baseUri += "/";
+
+			return new Uri(baseUri, UriKind.Absolute);
+		}
+
+		public IFile GetFile(string path)
+		{
+			var uri = new Uri(_baseUri, string.Format("FileSystem/File?path={0}", path));
+			try
+			{
+				var jsonData = _webClient.OpenRead(uri);
+				return _Deserialise<File>(jsonData);
+			}
+			catch (WebException exc)
+			{
+				_HandleWebException(exc, uri);
+				throw;
+			}
+		}
+
+		public IDirectory GetDirectory(string path)
+		{
+			var uri = new Uri(_baseUri, string.Format("FileSystem/Directory?path={0}", path));
+			try
+			{
+				var jsonData = _webClient.OpenRead(uri);
+				return _Deserialise<Directory>(jsonData);
+			}
+			catch (WebException exc)
+			{
+				_HandleWebException(exc, uri);
+				throw;
+			}
+		}
+
+		public IEnumerable<IFile> GetFiles(IDirectory directory, string searchPattern)
+		{
+			var uri = new Uri(_baseUri, string.Format("FileSystem/Files?directoryPath={0}&searchPattern={1}", directory.Name, searchPattern));
+			try
+			{
+				var jsonData = _webClient.OpenRead(uri);
+				return _Deserialise<File[]>(jsonData);
+			}
+			catch (WebException exc)
+			{
+				_HandleWebException(exc, uri);
+				throw;
+			}
+		}
+
+		public IEnumerable<IDirectory> GetDirectories(IDirectory directory)
+		{
+			var uri = new Uri(_baseUri, string.Format("FileSystem/Directories?directoryPath={0}", directory.Name));
+			try
+			{
+				var jsonData = _webClient.OpenRead(uri);
+				return _Deserialise<Directory[]>(jsonData);
+			}
+			catch (WebException exc)
+			{
+				_HandleWebException(exc, uri);
+				throw;
+			}
+		}
+
+		public Stream OpenRead(IFile file)
+		{
+			var uri = new Uri(_baseUri, string.Format("FileSystem/FileContent?path={0}", file.Name));
+			try
+			{
+				return _webClient.OpenRead(uri);
+			}
+			catch (WebException exc)
+			{
+				_HandleWebException(exc, uri);
+				throw;
+			}
+		}
+
+		public bool FileExists(string path)
+		{
+			try
+			{
+				GetFile(path);
+				return true;
+			}
+			catch (WebException)
+			{
+				return false;
+			}
+		}
+
+		public Stream OpenWrite(IFile file)
+		{
+			return new DelayedWriteStream(stream =>
+			{
+				var uri = new Uri(_baseUri, string.Format("FileSystem/FileContent?path={0}", file.Name));
+				try
+				{
+					var writeStream = _webClient.OpenWrite(uri, "POST");
+					stream.CopyTo(writeStream);
+					writeStream.Close();
+				}
+				catch (WebException exc)
+				{
+					_HandleWebException(exc, uri);
+					throw;
+				}
+			});
+		}
+
+		public IFile CreateFile(string path)
+		{
+			return new File(
+				Path.GetFileName(path),
+				new Directory(
+					Path.GetDirectoryName(path),
+					null,
+					this),
+				0,
+				DateTime.MinValue,
+				this
+				);
+		}
+
+		private T _Deserialise<T>(Stream jsonData)
+		{
+			using (var reader = new JsonTextReader(new StreamReader(jsonData)))
+				return _serialiser.Deserialize<T>(reader);
+		}
+
+		private static void _HandleWebException(WebException exception, Uri uri)
+		{
+			var httpResponse = exception.Response as HttpWebResponse;
+			if (httpResponse == null)
+				throw exception;
+
+			if (httpResponse.StatusCode == HttpStatusCode.NotFound)
+				throw new FileNotFoundException(uri.ToString());
+
+			if (httpResponse.StatusCode == HttpStatusCode.BadRequest)
+				throw new InvalidOperationException();
+
+			if (httpResponse.StatusCode == HttpStatusCode.LengthRequired)
+				throw new InvalidOperationException("No file data sent");
+
+			throw new InvalidOperationException("Unable to process request", exception);
+		}
+	}
+}
