c10216cbd7e00f8be95203f4120aa136e31bb379
Simon Laing <laingster@gmail.com>
2015-10-24 19:34:33 +0100

    Introduced trace statements for more transparency over single-task-gate

--- Models/FileSystem/AzureStorage/SingleTaskGate.cs
+++ Models/FileSystem/AzureStorage/SingleTaskGate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace FamilyTree.Models.FileSystem.AzureStorage
@@ -15,12 +17,17 @@ namespace FamilyTree.Models.FileSystem.AzureStorage
 			_timeout = timeout ?? TimeSpan.FromSeconds(2);
         }
 
-		public T Execute<T>(Func<T> action)
+		public T Execute<T>(Func<T> action, [CallerMemberName] string callerMethodName = "unknown")
 		{
+			Trace.TraceInformation("Waiting for slot for {0}", callerMethodName);
 			var signalIndex = WaitHandle.WaitAny(_signals, _timeout);
-            if (signalIndex == WaitHandle.WaitTimeout)
+			if (signalIndex == WaitHandle.WaitTimeout)
+			{
+				Trace.TraceWarning("Timeout waiting for slot for {0}", callerMethodName);
 				throw new TimeoutException();
+			}
 
+			Trace.TraceInformation("Slot acquired for {0}", callerMethodName);
 			var signal = _signals[signalIndex];
 			try
 			{
@@ -29,6 +36,7 @@ namespace FamilyTree.Models.FileSystem.AzureStorage
 			finally
 			{
 				signal.Set();
+				Trace.TraceInformation("Slot released for {0}", callerMethodName);
 			}
 		}
 	}
